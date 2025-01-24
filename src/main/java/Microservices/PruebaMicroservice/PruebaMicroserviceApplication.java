a2c82eb898789b248a8276bceaa654c0bf43bc7c
Angel Fernandez <angel.fernandez@multisafepay.com>
2025-01-24 16:43:58 +0100

    Primera prueba microservicio

---
+++ src/main/java/Microservices/PruebaMicroservice/PruebaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package Microservices.PruebaMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Microservices/PruebaMicroservice/PruebaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package Microservices.PruebaMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
